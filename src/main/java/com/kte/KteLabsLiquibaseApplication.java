001593e76690de4bde42caf0df60fa129a675c35
Сергей <robotic56@yandex.ru>
2023-01-23 20:23:28 +0300

    First commit

---
+++ src/main/java/com/kte/KteLabsLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.kte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class KteLabsLiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KteLabsLiquibaseApplication.class, args);
+    }
+}
